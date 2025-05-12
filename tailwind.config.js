d3f4c7cb9536b29171805c1cf0cfbb5e4c0e5cf7
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2025-05-12 12:18:28 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <- IMPORTANT LINE
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
